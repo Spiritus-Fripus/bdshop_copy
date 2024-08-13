50b1f5e263ecb17e9199d0690ea24fdb95306a56
Nathan Weber <81427220+Spiritus-Fripus@users.noreply.github.com>
2024-08-14 00:43:50 +0200

    changement dans la connection

--- admin/include/connect.php
+++ admin/include/connect.php
@@ -1,6 +1,9 @@
 <?php
+
+$config = require_once '.env.connect.php';
+
 try {
-    $db = new PDO('mysql:host=localhost;dbname=bdshop;charset=utf8', 'root', '');
+    $db = new PDO("mysql:host=" . $config['HOST'] . ";dbname=" . $config['DB_NAME'] . ";charset=utf8", $config['DB_USER'], $config['DB_PASSWORD']);
 } catch (PDOexception $e) {
     die($e->getMessage());
 }
